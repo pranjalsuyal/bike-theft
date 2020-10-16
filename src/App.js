00275521f9628af0d38173af7ad3866e4662d942
Pranjal Suyal <pranjal.suyal1503@gmail.com>
2020-10-16 23:38:14 +0530

    completed Navbar final touch needed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+// import Album from "./components/Album";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      {/* <Album /> */}
 
       <Switch>
         <Route path="/" exact />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,110 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import HomeIcon from "@material-ui/icons/Home";
+import { Home as HomeIcon, MoreVert as MoreIcon } from "@material-ui/icons";
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  colorWhite: {
+    color: "#FFF",
+    borderColor: "#FFF",
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+}));
 
 function Navbar() {
+  const [mobileAnchor, setMobileAnchor] = useState(null);
+  const classes = useStyles();
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileAnchor(event.currentTarget);
+  };
+  const handleMobileMenuClose = (event) => {
+    setMobileAnchor(null);
+  };
+  const isMobileMenuOpen = Boolean(mobileAnchor);
+
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileAnchor}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id="primary-search-account-menu-mobile"
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <Button fullWidth>Search Bikes</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button fullWidth>About</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button fullWidth>Help</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button fullWidth>Login</Button>
+      </MenuItem>
+    </Menu>
+  );
+
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            <HomeIcon /> BPD
-          </Link>
-        </div>
-      </nav>
+      <AppBar position="relative">
+        <Toolbar>
+          <HomeIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            BPD
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <Button className={classes.colorWhite}>Search Bikes</Button>
+            <Button className={classes.colorWhite}>About</Button>
+            <Button className={classes.colorWhite}>Help</Button>
+            <Button className={classes.colorWhite} variant="outlined">
+              Login
+            </Button>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls="primary-search-account-menu-mobile"
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
     </>
   );
 }

44d78015d72213d6a5ebb329e659dda82d24ead0
Pranjal Suyal <pranjal.suyal1503@gmail.com>
2020-10-19 02:53:39 +0530

    build added

---
+++ build/precache-manifest.1efbb885c8346a5590f109adf0549d88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d7fa6b620b9d6877b2126a5054f4cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "631f7a4aa8803ad5af48",
+    "url": "/static/js/2.5275400c.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.5275400c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f07b5dc3caf6e68368",
+    "url": "/static/js/main.9b17d2b6.chunk.js"
+  },
+  {
+    "revision": "5a2f188332ff5344e2e0",
+    "url": "/static/js/runtime-main.8d8ffe79.js"
+  },
+  {
+    "revision": "dd53f7d46d6fbf2717b98277833337ae",
+    "url": "/static/media/BPDLogo.dd53f7d4.png"
+  },
+  {
+    "revision": "ad711081055fe6b8f1757c5ff1cce0d9",
+    "url": "/static/media/bike.ad711081.png"
+  },
+  {
+    "revision": "6b7432e0b47569664de5a9a21b20b2a8",
+    "url": "/static/media/police.6b7432e0.jpg"
+  }
+]);
